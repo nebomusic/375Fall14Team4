4929858db66dac33f950f7930acff6fdc41bde41
Jack Winn <JackWinn18@marist.com>
2014-10-10 09:27:16 -0400

    Worked on the orders and main classes

--- CoffeeApp/src/com/example/coffeeapp/Main.java
+++ CoffeeApp/src/com/example/coffeeapp/Main.java
@@ -4,13 +4,59 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Main extends Activity {
+	// Field to hold Order data
+	private Orders orders;
+	private Drink curretnDrink;
+	
+	// Fields for User Interface Objects
+	private Button btnHot;
+	private Button btnCoffee;
+	private Button btnFrap;
+	private Button btnExpres;
+	private Button btnTall;
+	private Button btnGrande;
+	private Button btnVenti;
+	private Spinner spinnerFlavor;
+	private Spinner spinnerDairy;
+	private Button btnAddDrink;
+	private Button btnResetDrink;
+	private TextView textDrinksAdded;
+	private TextView textCurrentDrink;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Initialize Orders
+		orders = new Orders();
+		currentDrink = new Drink();
+		
+		// Bind to XML
+		btnHot = (Button)findViewById(R.id.btnHot);
+		btnCoffee = (Button)findViewById(R.id.btnCoffee);
+		btnFrap = (Button)findViewById(R.id.btnFrap);
+		btnExpres = (Button)findViewById(R.id.btnExpres);
+		btnTall = (Button)findViewById(R.id.btnTall);
+		btnGrande = (Button)findViewById(R.id.btnGrande);
+		btnVenti = (Button)findViewById(R.id.btnVenti);
+		spinnerFlavor = (Spinner)findViewById(R.id.spinnerFlavor);
+		spinnerDairy = (Spinner)findViewById(R.id.spinnerDairy);
+		btnAddDrink = (Button)findViewById(R.id.btnAddDrink);
+		btnResetDrink = (Button)findViewById(R.id.btnResetDrink);
+		textDrinksAdded = (TextView)findViewById(R.id.textDrinksAdded);
+		textCurrentDrink = (TextView)findViewById(R.id.textCurrentDrink); 
+		
+		// Populate the Spinner for Flavor
+		ArrayAdapter<CharSequence> flavorAdapter = ArrayAdapter.createFromResource(this,
+				R.array.flavor_array, android.R.layout.simple_spinner_dropdown_item);
+		
+		// Specify the layout to use when the list of choices appears
 	}
 
 	@Override
